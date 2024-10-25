dbc7e0f0735264cc890973d72d87c3855a7eed2b
gideonbruce <bruceotieno25@gmail.com>
2024-10-25 16:30:42 +0300

    Route to fetch nearby tourist attractions

--- sites.py
+++ sites.py
@@ -8,3 +8,8 @@ GOOGLE_API_KEY = 'your_google_api_key'
 
 # Initialize Google Maps client
 gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+
+@app.route("/tourism_sites", methods=["GET"])
+def get_tourism_sites():
+    location = request.args.get('location')  # Location can be passed as a query parameter
+    radius = request.args.get('radius', 5000)  # Optional radius parameter in meters (default is 5km)
